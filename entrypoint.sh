090a0098756f75e4bb48248e51fb6eb478d3aecf
Karwoo Tang <karwoo.tang@gmail.com>
2020-06-14 17:18:31 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -352,10 +352,10 @@ echo $log
 # get commit logs and determine home to bump the version
 # supports #major, #minor, #patch (anything else will be 'minor')
 case "$log" in
-    *#major* ) new=$(semver(bump, major, $tag));;
-    *#minor* ) new=$(semver(bump, minor, $tag));;
-    *#patch* ) new=$(semver(bump, patch, $tag));;
-    * ) new=$(semver(bump, `echo $default_semvar_bump`, $tag));;
+    *#major* ) new=$(semver bump major $tag);;
+    *#minor* ) new=$(semver bump minor $tag);;
+    *#patch* ) new=$(semver bump patch $tag);;
+    * ) new=$(semver bump `echo $default_semvar_bump` $tag);;
 esac
 
 # did we get a new tag?
